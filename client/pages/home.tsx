b56a37186bf6aff4a9748d5ec522cf93fbbeeb0a
ruslav <mateusz.gajo9@wp.pl>
2021-03-02 09:59:27 +0100

    add layout's sidebar

--- client/pages/home.tsx
+++ client/pages/home.tsx
@@ -1,22 +1,11 @@
 import React from "react";
 import MessageBox from "~common/messageBox/MessageBox";
 import HomeLayout from "~root/src/features/layout/homeLayout/HomeLayout";
-import setLanguage from "next-translate/setLanguage";
-import { setLanguage as sLang } from "~utils/utils";
 
 const home = () => {
-  const handleClick = () => {
-    const setLanguagee = async () => {
-      sLang("pl");
-      await setLanguage("pl");
-    };
-
-    setLanguagee();
-  };
   return (
     <section>
-      <button onClick={handleClick}>Click</button>
-      <HomeLayout children={<MessageBox />} />
+      <HomeLayout children={<MessageBox />} sidebar />
     </section>
   );
 };
